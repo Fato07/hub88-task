2ae59507f1cf1fbc47a4e515fef1118e04cefa81
fathin dosunmu <fathin.dosunmu@monese.com>
2022-11-19 16:59:30 +0200

    initialisation of project struct with graphql

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+import * as React from 'react'
+
+import CountryTable from './components/CountryTable';
+
+function App() {
+  return (
+    <div className="App">
+      This is the county Table
+      <CountryTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import './index.css';
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+);
+
